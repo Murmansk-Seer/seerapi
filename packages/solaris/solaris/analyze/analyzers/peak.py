8021f8394ad26ed9fe147419f45b2232464eda23
Nattsu39 <nattsu39@outlook.com>
2026-07-17 14:06:39 +0800

    refactor(solaris): 使用 ActivityTimeUpdateConfig 处理赛季编号和时间

--- packages/solaris/solaris/analyze/analyzers/peak.py
+++ packages/solaris/solaris/analyze/analyzers/peak.py
@@ -1,52 +1,54 @@
-from datetime import datetime, timedelta
-import re
+from datetime import datetime
 from typing import TYPE_CHECKING
 
 from seerapi_models.peak import PeakSeason
 from solaris.analyze.base import BaseDataSourceAnalyzer, DataImportConfig
 from solaris.analyze.typing_ import AnalyzeResult
-from solaris.utils import CN_TZ
+from solaris.parse.parsers.activity_time_update_config import ActivityTimeUpdateInfo
 
 if TYPE_CHECKING:
-    from solaris.parse.parsers.pvp_achieve import PvpAchieveConfig
-    from solaris.parse.parsers.pvp_task import PvpTaskConfig
+    pass
 
 
 class PeakSeasonAnalyzer(BaseDataSourceAnalyzer):
     @classmethod
     def get_data_import_config(cls) -> DataImportConfig:
-        return DataImportConfig(unity_paths=('pvpTask.json', 'pvpAchieve.json'))
+        return DataImportConfig(unity_paths=('activityTimeUpdateConfig.json',))
 
     @classmethod
     def get_result_res_models(cls):
         return (PeakSeason,)
 
     def analyze(self):
-        pvp_achieve_data: PvpAchieveConfig = self._get_data('unity', 'pvpAchieve.json')
-        season: str | None = None
-        for achieve in pvp_achieve_data['data']:
-            match = re.search(r'\d+', achieve.get('title', ''))
-            if match:
-                season = match.group()
+        activity_time_update_config_data: list[ActivityTimeUpdateInfo] = self._get_data(
+            'unity', 'activityTimeUpdateConfig.json'
+        )['root']['item']
+        season = None
+        start_time: datetime | None = None
+        end_time: datetime | None = None
+
+        for item in activity_time_update_config_data:
+            if item.get('id') == 1:
+                season = item.get('parameters1')
+                start_time = datetime.strptime(
+                    item.get('beginning'), '%Y_%m_%d %H:%M:%S'
+                )
+                end_time = datetime.strptime(item.get('ending'), '%Y_%m_%d %H:%M:%S')
                 break
+
         if not season:
             raise ValueError('未找到赛季编号')
-
-        pvp_task_data: PvpTaskConfig = self._get_data('unity', 'pvpTask.json')
-        start_time: datetime | None = None
-        for task in pvp_task_data['data']:
-            if task.get('id') == 1:
-                start_time = datetime.strptime(str(task['subkey']), '%Y%m%d')
-                start_time = start_time.replace(hour=10, tzinfo=CN_TZ)
-                break
 
         if not start_time:
             raise ValueError('未找到赛季开始时间')
 
+        if not end_time:
+            raise ValueError('未找到赛季结束时间')
+
         peak_season = PeakSeason(
             id=1,
             start_time=start_time,
-            end_time=start_time + timedelta(days=91),
+            end_time=end_time,
         )
         return (
             AnalyzeResult(
